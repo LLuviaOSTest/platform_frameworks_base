f9325b9bf667b24504c30754a4c5843d98b33753
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    base: Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- core/java/com/android/internal/util/yodita/YoditaUtils.java
+++ core/java/com/android/internal/util/yodita/YoditaUtils.java
@@ -18,10 +18,12 @@ package com.android.internal.util.yodita;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.util.DisplayMetrics;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -67,5 +69,24 @@ public class YoditaUtils {
                 }
             }
         }
+    }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
