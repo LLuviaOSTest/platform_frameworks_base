Havoc-OS/android_frameworks_base

base: Fix merge derp

packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -374,14 +374,14 @@ public class NotificationInterruptionStateProvider {
         if (mSkipHeadsUp) {
             boolean isNonInstrusive = notificationPackageName.contains("dialer") ||
                 notificationPackageName.contains("clock");
-            return mSkipHeadsUp && !isNonInstrusive;
+            return !mStatusBarStateController.isDozing() && mSkipHeadsUp && !isNonInstrusive;
         }
 
         boolean isLessBoring = notificationPackageName.contains("dialer") ||
                 notificationPackageName.contains("clock") ||
                 notificationPackageName.contains("messaging");
 
-        return mLessBoringHeadsUp && !isLessBoring;
+        return !mStatusBarStateController.isDozing() && mLessBoringHeadsUp && !isLessBoring;
     }
 
     /**

LLuviaOSTest/platform_frameworks_base

Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -367,7 +367,7 @@ public class NotificationInterruptionStateProvider {
         isImportantHeadsUp = notificationPackageName.contains("dialer") ||
                 notificationPackageName.contains("messaging") ||
                 notificationPackageName.contains("clock");
-        return !getShadeController().isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
+        return !mStatusBarStateController.isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
     }
 
     /**